fb3c476b06e1fda05bebc8fe8c8c28195cef7588
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2022-02-25 12:35:11 -0800

    Fixed get account statement

--- sql/query_account.sql
+++ sql/query_account.sql
@@ -9,26 +9,29 @@ CREATE OR REPLACE FUNCTION get_account_with_commitment_level(
 )
 RETURNS SETOF account
 LANGUAGE plpgsql
-AS $$
+AS
+$$
 BEGIN
 
     SELECT * FROM account AS acct
     JOIN slot AS s ON acct.slot = s.slot
-    WHERE acct.pubkey = $1
-    AND s.status = $2;
+    WHERE acct.pubkey = pubkey
+    AND s.status = commitment_level;
     EXCEPTION
         WHEN no_data_found THEN BEGIN
             SELECT * FROM account_audit AS acct1
             JOIN slot AS s ON acct1.slot = s.slot
-            WHERE acct.pubkey = $1
-            AND s.status = $2;
-            WHERE NOT EXISTS (
+            WHERE acct.pubkey = pubkey
+            AND s.status = commitment_level
+            AND NOT EXISTS (
                 SELECT NULL FROM account_audit AS acct2
                 JOIN slot AS s2 ON acct2.slot = s2.slot
                 WHERE s.slot < s2.slot
-                AND acct2.pubkey = $1
-                AND s2.status = $2
+                AND acct2.pubkey = pubkey
+                AND s2.status = commitment_level
             );
         END;
-END; $$
+END;
+
+$$;
 
