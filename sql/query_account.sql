de959ae245d458ec8fd14ac8017cdc25a493206e
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2022-02-25 17:58:01 -0800

    WIP for the query account stored function

--- sql/query_account.sql
+++ sql/query_account.sql
@@ -4,33 +4,37 @@
 
 
 CREATE OR REPLACE FUNCTION get_account_with_commitment_level(
-    pubkey BYTEA,
+    input_pubkey BYTEA,
     commitment_level VARCHAR(16)
 )
-RETURNS SETOF account
+RETURNS RECORD
 LANGUAGE plpgsql
 AS
 $$
+DECLARE
+ret RECORD;
 BEGIN
 
-    SELECT * FROM account AS acct
+    SELECT acct.* FROM account AS acct
     JOIN slot AS s ON acct.slot = s.slot
-    WHERE acct.pubkey = pubkey
-    AND s.status = commitment_level;
+    WHERE acct.pubkey = input_pubkey
+    AND s.status = commitment_level INTO ret;
     EXCEPTION
         WHEN no_data_found THEN BEGIN
             SELECT * FROM account_audit AS acct1
             JOIN slot AS s ON acct1.slot = s.slot
-            WHERE acct.pubkey = pubkey
+            WHERE acct1.pubkey = input_pubkey
             AND s.status = commitment_level
             AND NOT EXISTS (
                 SELECT NULL FROM account_audit AS acct2
                 JOIN slot AS s2 ON acct2.slot = s2.slot
                 WHERE s.slot < s2.slot
-                AND acct2.pubkey = pubkey
+                AND acct2.pubkey = input_pubkey
                 AND s2.status = commitment_level
-            );
+            )
+            INTO ret;
         END;
+    RETURN ret;
 END;
 
 $$;
