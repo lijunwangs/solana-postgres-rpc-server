44259ea1cb07c9c101e591931667619e1a194b7b
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2022-03-04 15:43:48 -0800

    Implementing get_account_with_commitment_level_and_slot SQL function

--- sql/query_account.sql
+++ sql/query_account.sql
@@ -38,3 +38,42 @@ END;
 
 $$;
 
+
+CREATE OR REPLACE FUNCTION get_account_with_commitment_level_and_slot(
+    input_pubkey BYTEA,
+    commitment_level VARCHAR(16),
+    max_slot BIGINT
+)
+RETURNS RECORD
+LANGUAGE plpgsql
+AS
+$$
+DECLARE
+ret RECORD;
+BEGIN
+    SELECT acct.* FROM account AS acct
+    JOIN slot AS s ON acct.slot = s.slot
+    WHERE acct.pubkey = input_pubkey
+    AND s.slot <= max_slot
+    AND s.status = commitment_level INTO ret;
+
+    IF NOT FOUND THEN
+        SELECT acct1.* FROM account_audit AS acct1
+        JOIN slot AS s ON acct1.slot = s.slot
+        WHERE acct1.pubkey = input_pubkey
+        AND s.slot <= max_slot
+        AND s.status = commitment_level
+        AND NOT EXISTS (
+            SELECT NULL FROM account_audit AS acct2
+            JOIN slot AS s2 ON acct2.slot = s2.slot
+            WHERE s.slot < s2.slot
+            AND acct2.pubkey = input_pubkey
+            AND s2.status = commitment_level
+            AND s2.slot <= max_slot
+        )
+        INTO ret;
+    END IF;
+    RETURN ret;
+END;
+
+$$;
